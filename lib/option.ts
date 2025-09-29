18d99e28646887c1248086b04634e0fe1fc3a264
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2025-09-29 22:37:30 +0200

    feat(option.ts): assert{Defined,NotNull}()
    Co-authored-by: Ignacy Smoliński <149937144+leadman5555@users.noreply.github.com>

--- lib/option.ts
+++ lib/option.ts
@@ -36,3 +36,43 @@ export function nullableMap<T, R>(
 ): R | null {
   return input === null ? null : f(input);
 }
+
+/**
+ * Asserts that the provided optional value is not `undefined`
+ *
+ * Basically Rust's [Option::unwrap](https://doc.rust-lang.org/std/option/enum.Option.html#method.unwrap) ported to JS.
+ * If the input value is undefined, throws an error.
+ * Otherwise, returns the value unchanged.
+ *
+ * @param value the input value
+ * @param [valueName="value"] a description of the value, used in the thrown error
+ * @returns the input value unchanged
+ * @throws TypeError "Expected ${valueName} to be defined" if the value is undefined
+ */
+export function assertDefined<T>(value: T | undefined, valueName: string = "value"): T {
+  if (value === undefined) {
+    throw new TypeError(`Expected ${valueName} to be defined`);
+  }
+  return value;
+}
+
+/**
+ * Asserts that the provided nullable value is not `null`
+ *
+ * Basically Rust's [Option::unwrap](https://doc.rust-lang.org/std/option/enum.Option.html#method.unwrap) ported to JS.
+ * This one's for nullables, because apparently JS chose to have 2 "null" values instead of the correct number - 0.
+ * If the input value is null, immediately retuns null.
+ * If the input value is undefined, throws an error.
+ * Otherwise, returns the value unchanged.
+ *
+ * @param value the input value
+ * @param [valueName="value"] a description of the value, used in the thrown error
+ * @returns the input value unchanged
+ * @throws TypeError "Expected ${valueName} to not be null" if the value is null
+ */
+export function assertNotNull<T>(value: T | undefined, valueName: string = "value"): T {
+  if (value === undefined) {
+    throw new TypeError(`Expected ${valueName} to not be null`);
+  }
+  return value;
+}
