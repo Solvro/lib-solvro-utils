90bf55a39c011bdee1348a790835b14f85d4628f
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2025-09-30 13:48:43 +0200

    chore: add copyright headers

--- test/map.test.ts
+++ test/map.test.ts
@@ -1,3 +1,5 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import { ExtendedMap, extendGlobally, extendMap } from "../lib/map.ts";
 
 describe("map.ts", () => {
--- test/support/setup.ts
+++ test/support/setup.ts
@@ -1,3 +1,5 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import { use } from "chai";
 import chaiAsPromised from "chai-as-promised";
 import spies from "chai-spies";
