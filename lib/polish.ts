1a669db75fad33376a7c17cb7eb9e84febdf79db
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2026-09-04 14:43:19 +0200

    refactor(polish): convert to class, add more tests

--- lib/polish.ts
+++ lib/polish.ts
@@ -2,43 +2,61 @@
 // If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 /**
- * Prepares a quantitative noun for declension based on its count.
- * @param singular - The singular form of the noun (used for 1).
- * @param paucal - The paucal form of the noun (used for 2, 3, 4).
- * @param plural - The plural form of the noun (used for 0, 5 and above).
- * @returns an object containing a function used to decline the noun numerically.
- * @example const declineApple = quantitativeNoun('jabłko', 'jabłka', 'jabłek');
- * @example declineApple.declineNumeric(1) // '1 jabłko'
- * @example declineApple.declineNumeric(2) // '2 jabłka'
- * @example declineApple.declineNumeric(5) // '5 jabłek'
+ * An object representing a noun prepared for declension based on its count.
+ *
+ * Contains strings for the 3 possible forms of the noun,
+ * and a method to pick the right form based on the count.
+ *
+ * @example
+ * const apple = new QuantitativeNoun('jabłko', 'jabłka', 'jabłek');
+ * apple.decline(1) // '1 jabłko'
+ * apple.decline(2) // '2 jabłka'
+ * apple.decline(5) // '5 jabłek'
+ * apple.singluar   // 'jabłko'
+ * apple.paucal     // 'jabłka'
+ * apple.plural     // 'jabłek'
  */
-export function quantitativeNoun(
-  singular: string,
-  paucal: string,
-  plural: string,
-) {
+export class QuantitativeNoun {
+  /**
+   * The singular form of the noun (used for 1).
+   */
+  public readonly singular: string;
+  /**
+   * The paucal form of the noun (used for 2, 3, 4).
+   */
+  public readonly paucal: string;
+  /**
+   * The plural form of the noun (used for 0, 5 and above).
+   */
+  public readonly plural: string;
+
+  public constructor(singular: string, paucal: string, plural: string) {
+    this.singular = singular;
+    this.paucal = paucal;
+    this.plural = plural;
+  }
+
   /**
    * Declines a quantitative noun based on its count using manually-provided grammatical forms.
    * @param count - The numeric count.
    * @returns The declined noun with the count as a space-separated string.
-   * @example quantitativeNoun('jabłko', 'jabłka', 'jabłek').declineNumeric(1) // '1 jabłko'
-   * @example quantitativeNoun('jabłko', 'jabłka', 'jabłek').declineNumeric(2) // '2 jabłka'
-   * @example quantitativeNoun('jabłko', 'jabłka', 'jabłek').declineNumeric(5) // '5 jabłek'
+   * @example
+   * const apple = new QuantitativeNoun('jabłko', 'jabłka', 'jabłek');
+   * apple.decline(1) // '1 jabłko'
+   * apple.decline(2) // '2 jabłka'
+   * apple.decline(5) // '5 jabłek'
    */
-  function declineNumeric(count: number): string {
-    const countString = count.toString();
+  public decline(count: number): string {
     if (count === 1) {
-      return `${countString} ${singular}`;
+      return `${count} ${this.singular}`;
     }
     const lastTwoDigits = count % 100;
     if (lastTwoDigits >= 12 && lastTwoDigits <= 14) {
       // Exception for numbers ending with 12, 13 and 14, which always use the plural-many form
-      return `${countString} ${plural}`;
+      return `${count} ${this.plural}`;
     }
     const lastDigit = count % 10;
     const isMany = lastDigit <= 1 || lastDigit >= 5;
-    return `${countString} ${isMany ? plural : paucal}`;
+    return `${count} ${isMany ? this.plural : this.paucal}`;
   }
-
-  return { declineNumeric };
 }
--- test/polish.test.ts
+++ test/polish.test.ts
@@ -1,47 +1,104 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 // If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
-import { quantitativeNoun } from "../lib/polish.ts";
+import { QuantitativeNoun } from "../lib/polish.ts";
 
 describe("polish.ts", () => {
-  describe("quantitativeNoun", () => {
-    it("should handle normal cases", () => {
-      const category = quantitativeNoun("kategoria", "kategorie", "kategorii");
-      expect(category.declineNumeric(0)).to.equal("0 kategorii");
-      expect(category.declineNumeric(1)).to.equal("1 kategoria");
-      expect(category.declineNumeric(2)).to.equal("2 kategorie");
-      expect(category.declineNumeric(5)).to.equal("5 kategorii");
+  describe("QuantitativeNoun", () => {
+    describe(".decline", () => {
+      it("should handle normal cases", () => {
+        const category = new QuantitativeNoun(
+          "kategoria",
+          "kategorie",
+          "kategorii",
+        );
+        expect(category.decline(0)).to.equal("0 kategorii");
+        expect(category.decline(1)).to.equal("1 kategoria");
+        expect(category.decline(2)).to.equal("2 kategorie");
+        expect(category.decline(5)).to.equal("5 kategorii");
 
-      const option = quantitativeNoun("opcja", "opcje", "opcji");
-      expect(option.declineNumeric(0)).to.equal("0 opcji");
-      expect(option.declineNumeric(1)).to.equal("1 opcja");
-      expect(option.declineNumeric(2)).to.equal("2 opcje");
-      expect(option.declineNumeric(3)).to.equal("3 opcje");
-      expect(option.declineNumeric(4)).to.equal("4 opcje");
-      expect(option.declineNumeric(5)).to.equal("5 opcji");
-      expect(option.declineNumeric(11)).to.equal("11 opcji");
-      expect(option.declineNumeric(15)).to.equal("15 opcji");
-      expect(option.declineNumeric(21)).to.equal("21 opcji");
-      expect(option.declineNumeric(22)).to.equal("22 opcje");
+        const option = new QuantitativeNoun("opcja", "opcje", "opcji");
+        expect(option.decline(0)).to.equal("0 opcji");
+        expect(option.decline(1)).to.equal("1 opcja");
+        expect(option.decline(2)).to.equal("2 opcje");
+        expect(option.decline(3)).to.equal("3 opcje");
+        expect(option.decline(4)).to.equal("4 opcje");
+        expect(option.decline(5)).to.equal("5 opcji");
+        expect(option.decline(11)).to.equal("11 opcji");
+        expect(option.decline(15)).to.equal("15 opcji");
+        expect(option.decline(21)).to.equal("21 opcji");
+        expect(option.decline(22)).to.equal("22 opcje");
+      });
+
+      it("should handle exception cases", () => {
+        const category = new QuantitativeNoun(
+          "kategoria",
+          "kategorie",
+          "kategorii",
+        );
+        expect(category.decline(12)).to.equal("12 kategorii");
+        expect(category.decline(22)).to.equal("22 kategorie");
+        expect(category.decline(113)).to.equal("113 kategorii");
+        expect(category.decline(123)).to.equal("123 kategorie");
+
+        const option = new QuantitativeNoun("opcja", "opcje", "opcji");
+        expect(option.decline(12)).to.equal("12 opcji");
+        expect(option.decline(13)).to.equal("13 opcji");
+        expect(option.decline(14)).to.equal("14 opcji");
+      });
     });
 
-    it("should handle exception cases", () => {
-      const category = quantitativeNoun("kategoria", "kategorie", "kategorii");
-      expect(category.declineNumeric(12)).to.equal("12 kategorii");
-      expect(category.declineNumeric(22)).to.equal("22 kategorie");
-      expect(category.declineNumeric(113)).to.equal("113 kategorii");
-      expect(category.declineNumeric(123)).to.equal("123 kategorie");
+    describe(".singular/.paucal/.plural", () => {
+      it("configured strings should be accessible", () => {
+        const category = new QuantitativeNoun(
+          "kategoria",
+          "kategorie",
+          "kategorii",
+        );
+        expect(category.singular).to.equal("kategoria");
+        expect(category.paucal).to.equal("kategorie");
+        expect(category.plural).to.equal("kategorii");
 
-      const option = quantitativeNoun("opcja", "opcje", "opcji");
-      expect(option.declineNumeric(12)).to.equal("12 opcji");
-      expect(option.declineNumeric(13)).to.equal("13 opcji");
-      expect(option.declineNumeric(14)).to.equal("14 opcji");
+        const option = new QuantitativeNoun("opcja", "opcje", "opcji");
+        expect(option.singular).to.equal("opcja");
+        expect(option.paucal).to.equal("opcje");
+        expect(option.plural).to.equal("opcji");
+      });
+
+      it("modifying strings after creation should cause TS errors", function () {
+        this.slow(2000);
+        this.timeout(5000);
+
+        expect(`
+          import {QuantitativeNoun} from "../lib/polish.ts";
+
+          const option = new QuantitativeNoun("opcja", "opcje", "opcji");
+          option.singular = "kategoria";
+        `).to.not.compile();
+
+        expect(`
+          import {QuantitativeNoun} from "../lib/polish.ts";
+
+          const option = new QuantitativeNoun("opcja", "opcje", "opcji");
+          option.paucal = "kategorie";
+        `).to.not.compile();
+
+        expect(`
+          import {QuantitativeNoun} from "../lib/polish.ts";
+
+          const option = new QuantitativeNoun("opcja", "opcje", "opcji");
+          option.plural = "kategorii";
+        `).to.not.compile();
+      });
     });
 
     it("should work with the JSDoc apple example", () => {
-      const apple = quantitativeNoun("jabłko", "jabłka", "jabłek");
-      expect(apple.declineNumeric(1)).to.equal("1 jabłko");
-      expect(apple.declineNumeric(2)).to.equal("2 jabłka");
-      expect(apple.declineNumeric(5)).to.equal("5 jabłek");
+      const apple = new QuantitativeNoun("jabłko", "jabłka", "jabłek");
+      expect(apple.decline(1)).to.equal("1 jabłko");
+      expect(apple.decline(2)).to.equal("2 jabłka");
+      expect(apple.decline(5)).to.equal("5 jabłek");
+      expect(apple.singular).to.equal("jabłko");
+      expect(apple.paucal).to.equal("jabłka");
+      expect(apple.plural).to.equal("jabłek");
     });
   });
 });
